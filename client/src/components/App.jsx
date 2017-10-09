bd564e6d4d8ae74dbc003e7efd6890b7ec8e7220
Nat Hamilton <info@nathamilton.com>
2017-10-08 22:00:28 -0500

    added in svg, started a few basic components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
+import { Header } from './Header.jsx'
 
 export const App = () => {
-  let title = 'Plantera'
   return (
-    <h1>{ title }</h1>
+    <Header />
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Header = () => (
+  <header>
+    <h2 className='tx--primary'>Plantera</h2>
+  </header>
+)
