8363db221beefb6367eccbb5e2d300b33af4718c
Nat Hamilton <info@nathamilton.com>
2017-10-08 22:40:52 -0500

    mainly testing my new emacs configs

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export const Header = () => (
-  <header>
-    <h2 className='tx--primary'>Plantera</h2>
+  <header className='bdr--b bdr--gray1'>
+    <div className='cn cn2 gp'>
+      <h2 className='tx--primary tx--reset'>Plantera</h2>
+
+      <div>
+        <button className='btn btn--primary pd0'>
+          <span className='sr'>
+            Add new plant
+          </span>
+          <svg className='icon icon-add'>
+            <use xlinkHref='#icon-add' />
+          </svg>
+        </button>
+      </div>
+    </div>
   </header>
 )
