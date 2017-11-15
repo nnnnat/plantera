7633802c16e64922dc2468eee29ccc470fca8702
Nat Hamilton <info@nathamilton.com>
2017-11-15 15:39:01 -0600

    updated layout css

--- client/src/components/views/plantForm.jsx
+++ client/src/components/views/plantForm.jsx
@@ -3,12 +3,8 @@ import React from 'react'
 import PageTitle from './../elements/pageTitle'
 import Form from './../blocks/form'
 
-const css = {
-  minHeight: 'calc(100vh - 105px)'
-}
-
 const PlantForm = ({ plant }) => (
-  <section className='form bdr--t bdr--success bg--gray0' style={css}>
+  <section className='form bdr--t bdr--success bg--gray0'>
     <PageTitle title={(plant) ? 'Edit Plant' : 'Add Plant'} />
     <div className='cn2 pd2--x mg--auto'>
       <Form edit={plant} />
